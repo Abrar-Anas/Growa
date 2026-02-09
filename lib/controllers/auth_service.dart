502bdbe9bcc670d737bd6ecf14154b7d878da2a9
Abrar Anas <abraranas4321@gmail.com>
2026-02-09 10:41:43 +0530

    hello

--- lib/controllers/auth_service.dart
+++ lib/controllers/auth_service.dart
@@ -1,37 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
-class AuthService extends StatefulWidget {
-  const AuthService({super.key});
-
-  @override
-  State<AuthService> createState() => _AuthServiceState();
-}
-
-class _AuthServiceState extends State<AuthService> {
-  List post = [];
-
-  void fetchPosts() async {
-    try {
-      var respone = await Dio().get("https://dummyjson.com/auth/login");
-      setState(() {
-        post = respone.data;
-      });
-    } catch (e) {
-      print('Error $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: post.length,
-        itemBuilder: (context, index) {
-          return ListTile(title: Text(post[index]['title']),
-          subtitle: Text(post[index]['body']),);
-        },
-      ),
-    );
-  }
+class ApiService {
+  final Dio _dio = Dio(
+    BaseOptions(baseUrl: "growa.nexariccreations.store",
+    headers: {
+      'Content-Type': 'application/json',
+      ''
+    }));
 }
